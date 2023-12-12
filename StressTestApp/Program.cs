34aa172fd2769b6ff9a0a3b961b027873c51c6f4
Nataliia Balenko <Nataliia_Balenko@epam.com>
2023-12-12 17:11:54 +0200

    Refactor stress test parameters and loop functionality (fixed When.All)
    The code has been modified to adjust the stress test parameters, reducing the number of requests per thread but increasing the number of threads. The mechanism for looping through threads and each thread's requests has been refactored for efficiency. The program was also transitioned from partial class to standalone class.

--- StressTestApp/Program.cs
+++ StressTestApp/Program.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using StressTestApp.Models;
 
-partial class Program
+class Program
 {
     static readonly HttpClient httpClient = new HttpClient();
 
@@ -10,12 +10,17 @@ partial class Program
     {
         var stressTestParams = new StressTestParams
         {
-            ThreadCount = 5,
-            RequestPerThread = 10,
-            BaseUrlWithPathToPage = "http://epam-igm-qa-orch-service-alb-1240739889.us-east-2.elb.amazonaws.com/api/v1/stories?per=1&page=",
+            ThreadCount = 15,
+            RequestPerThread = 5,
+            BaseUrlWithPathToPage = "http://epam-igm-qa-orch-service-alb-1240739889.us-east-2.elb.amazonaws.com/api/v1/stories?per=100&page=",
             AuthToken = "Token token=aa601ab42270edc4433f878cac861e50, company_key=8bc9f1b2-b862-48f3-90a6-7b0391899ec6"
         };
-  
+
+        await RunStressTestAndCalculateStatistic(stressTestParams);
+    }
+
+    private static async Task RunStressTestAndCalculateStatistic(StressTestParams stressTestParams)
+    {
         var datetimeStart = DateTime.Now;
         var requestStatistics = await RunStressTest(stressTestParams);
         var datetimeEnd = DateTime.Now;
@@ -31,11 +36,11 @@ partial class Program
         var requestStatistics = new ConcurrentBag<ResponseInfo>();
         for (var threadNumber = 1; threadNumber <= stressTestParams.ThreadCount; threadNumber++)
         {
-            for (var i = 1; i <= stressTestParams.RequestPerThread; i++)
-            {
-                allTasks.Add(ProcessUrlAsync(stressTestParams.BaseUrlWithPathToPage, threadNumber, i, requestStatistics, stressTestParams.ThreadCount));
-            }
+            allTasks.Add(ProcessUrlAsync(stressTestParams.BaseUrlWithPathToPage, threadNumber,
+                stressTestParams.RequestPerThread, requestStatistics,
+                stressTestParams.ThreadCount));
         }
+
         await Task.WhenAll(allTasks);
 
         return requestStatistics;
@@ -58,30 +63,33 @@ partial class Program
         Console.WriteLine(totalReport);
     }
 
-    static async Task ProcessUrlAsync(string baseUrl, int threadNumber, int iteration, 
+    static async Task ProcessUrlAsync(string baseUrl, int threadNumber, int requestsPerThread,
         ConcurrentBag<ResponseInfo> requestStatistics, int threads = 10)
     {
-        int pageNum = (threadNumber - 1) * threads * 10 + iteration;
-        string url = baseUrl + pageNum;
-        Console.WriteLine($"{threadNumber} {iteration} {url} - started");
-        
-        Stopwatch stopwatch = Stopwatch.StartNew();
-
-        try
+        for (var iteration = 1; iteration <= requestsPerThread; iteration++)
         {
-            var response = await httpClient.GetAsync(url);
-            stopwatch.Stop();
-            var time = stopwatch.ElapsedMilliseconds;
+            var pageNum = (threadNumber - 1) * threads * 10 + iteration;
+            var url = baseUrl + pageNum;
+            Console.WriteLine($"{threadNumber} {iteration} {url} - started");
 
-            Console.WriteLine(response.IsSuccessStatusCode
-                ? $"{threadNumber} {iteration} {url} - finished: {response.StatusCode} in {time} ms"
-                : $"{threadNumber} {iteration} {url} - failed with status code: {response.StatusCode} in {time} ms");
+            var stopwatch = Stopwatch.StartNew();
 
-            requestStatistics.Add(new ResponseInfo(response, time, threadNumber, iteration, pageNum));
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"{threadNumber} {iteration} {url} failed: {ex.Message}");
+            try
+            {
+                var response = await httpClient.GetAsync(url);
+                stopwatch.Stop();
+                var time = stopwatch.ElapsedMilliseconds;
+
+                Console.WriteLine(response.IsSuccessStatusCode
+                    ? $"{threadNumber} {iteration} {url} - finished: {response.StatusCode} in {time} ms"
+                    : $"{threadNumber} {iteration} {url} - failed with status code: {response.StatusCode} in {time} ms");
+
+                requestStatistics.Add(new ResponseInfo(response, time, threadNumber, iteration, pageNum));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{threadNumber} {iteration} {url} failed: {ex.Message}");
+            }
         }
     }
 }
